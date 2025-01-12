6ed1b338007b044c3278388306cd634a15243aca
Igor Achkas <jigoren@yandex.ru>
2025-01-12 15:29:27 +0700

    Add start

---
+++ Source/LeaveMeAlone/Public/LMAGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LMAGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAVEMEALONE_API ALMAGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
